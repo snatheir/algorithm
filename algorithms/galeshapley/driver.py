ae768675cc4d3f099334a5043a7316d88a9f54a3
Dima Ismail <dismail@uoguelph.ca>
2020-08-09 13:17:09 -0400

    cleaned up code for readability

--- algorithms/galeshapley/driver.py
+++ algorithms/galeshapley/driver.py
@@ -1,14 +1,5 @@
 import galeshapley
 import json
-# import os
-
-# cwd = os.getcwd()
-# list = os.listdir()
-# print(cwd)
-# print(list)
-
 
 sampleJson = open("util/sample.json", "r")
-# print(type(sampleJson))
-# json.loads(sampleJson)
 galeshapley.main(sampleJson)
