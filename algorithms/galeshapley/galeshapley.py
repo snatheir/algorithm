f9ca561f6753b5eb89acd20c0aee85c98e9ea833
Dima Ismail <dismail@uoguelph.ca>
2020-08-09 13:17:49 -0400

    implemented galeshapley, tested w sample input

--- algorithms/galeshapley/galeshapley.py
+++ algorithms/galeshapley/galeshapley.py
@@ -1,6 +1,14 @@
-def galeshapley(htmlInput):
-    A = htmlInput[0]
-    B = htmlInput[1]
-    print(A, B)
-    matching = galeshapley(A, B)
+from matching.algorithms import galeshapley
+import json
+
+def main(wrappedInput):
+    stringInput = wrappedInput.read()
+    jsonInput = json.loads(stringInput)
+    print(jsonInput['author'])
+    menSuitors = jsonInput['sample_input']['men']
+    womenSuitors = jsonInput['sample_input']['women']
+    matching = galeshapley(menSuitors, womenSuitors)
     print(matching)
+
+if __name__ == "__main__":
+    main(jsonInput)
